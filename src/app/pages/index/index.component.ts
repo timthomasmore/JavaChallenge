fb03c6ea4f699fc97a3a05e4cd67f1e21782719f
Joren97 <jorensynaeve@hotmail.com>
2018-11-19 11:38:16 +0100

    dashboard layout klaar

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -12,9 +12,20 @@ export class IndexComponent implements OnInit {
 
   public AnimationBarOption;
 
-  constructor(private _chartsService: ChartsService) { }
+  totalUsers;
+  totalScoredPoints;
+  totalBoughtRewards;
+  BarOptionCustom;
+  monthlyPoints = [];
+
+  constructor(private _chartsService: ChartsService) {}
 
   ngOnInit() {
+    this.monthlyPoints = [10, 52, 200, 334, 500, 330, 500, 600, 500, 250, 389, 829]; // Te vervangen door api call
+    this.BarOptionCustom = this._chartsService.getBarOptionCustom(this.monthlyPoints);
     this.AnimationBarOption = this._chartsService.getAnimationBarOption();
+    this.totalUsers = 214; // Te vervangen door api call
+    this.totalScoredPoints = 1500; // Te vervangen door api call
+    this.totalBoughtRewards = 137; // Te vervangen door api call
   }
 }
