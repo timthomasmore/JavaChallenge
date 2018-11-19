b037a5f66ac6a2a17977fe1e2cd0a8b0b4d4bc64
timthomasmore <33808361+timthomasmore@users.noreply.github.com>
2018-11-19 15:03:33 +0100

    Menu cleanup

--- src/app/pages/menu.ts
+++ src/app/pages/menu.ts
@@ -117,25 +117,4 @@ export let MENU_ITEM = [
             }
         ]
     },
-    {
-        path: 'menu-levels',
-        title: 'Menu Levels',
-        icon: 'sitemap',
-        children: [
-            {
-                path: 'levels1',
-                title: 'Menu Level1',
-                children: [
-                    {
-                        path: 'levels1-1',
-                        title: 'Menu Level1-1'
-                    }
-                ]
-            },
-            {
-                path: 'levels2',
-                title: 'Menu Level2'
-            }
-        ]
-    },
 ];
