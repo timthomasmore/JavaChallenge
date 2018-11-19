f53e79e327f593c536112b757494a03070c4b270
jordibols <r0631689@student.thomasmore.be>
2018-11-19 15:10:13 +0100

    Added custom chart support

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartsService } from '../../shared/services/charts.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  providers: [ChartsService]
 })
 export class ProfileComponent implements OnInit {
 
@@ -23,9 +25,37 @@ export class ProfileComponent implements OnInit {
     {name: 'Andere', date: new Date(), points: 8}
   ];
 
-  constructor() { 
+  showloading = false;
+  BarOption;
+  LineOption;
+  PieOption;
+  AnimationBarOption;
+
+  constructor(private chartsService: ChartsService) { 
     this.activityList = this.activityList.splice(0, 5);
     this.rewardList = this.rewardList.splice(0, 5);
+
+    let data1 = [];
+    let data2 = [];
+    let xAxisData = [];
+    let pieData = [];
+    let pieLegend = [];
+
+    // EYY
+    for (let i = 0; i < 12; i++) {
+      data1.push( Math.floor( Math.random() * 120 ) );
+      data2.push( Math.floor( Math.random() * 120 ) );
+      xAxisData.push('Val ' + ( i + 1 ) );
+      pieData.push( { value: data1[i], name: 'Val ' + i } );
+      pieLegend.push('Val ' + i);
+    }
+
+    // EINDE EYY
+
+    this.BarOption = this.chartsService.getBarOption(data1, xAxisData);
+    this.LineOption = this.chartsService.getLineOption(data1, xAxisData);
+    this.PieOption = this.chartsService.getPieOption(pieData, pieLegend);
+    this.AnimationBarOption = this.chartsService.getAnimationBarOption(data1, data2, xAxisData);
   }
 
   ngOnInit() { }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { routing } from './profile.routing';
 import { SharedModule } from '../../shared/shared.module';
 import { ProfileComponent } from './profile.component';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
-        routing
+        routing,
+        NgxEchartsModule
     ],
     declarations: [
         ProfileComponent
