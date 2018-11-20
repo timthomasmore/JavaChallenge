b38eb3c4725b5b0aa52fb91763d29605b0a4837a
Joren97 <jorensynaeve@hotmail.com>
2018-11-20 09:54:19 +0100

    joren

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RestService} from '../../shared/services/rest.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  auth = "show";
-  reset = "hide";
-  resetPasswordAPI = "www.url.com/testURL"; //TE VERVANGEN DOOR LOGIN API
-  loginAPI = "www.url.com/testLOGIN"; //TE VERVANGEN DOOR LOGIN API
+  auth = 'show';
+  reset = 'hide';
+  resetPasswordAPI = 'www.url.com/testURL'; //TE VERVANGEN DOOR LOGIN API
+  loginAPI = 'www.url.com/testLOGIN'; //TE VERVANGEN DOOR LOGIN API
 
   user = {
     name: '',
@@ -17,37 +18,38 @@ export class LoginComponent implements OnInit {
     email: ''
   };
 
-  constructor() { }
+  constructor(private restService: RestService) {
+  }
 
   ngOnInit() {
   }
 
-  forgotPassword(){
-    this.auth = "hide";
-    this.reset = "show";
+  forgotPassword() {
+    this.auth = 'hide';
+    this.reset = 'show';
   }
 
-   back(){
-     this.auth = "show";
-     this.reset = "hide";
-     this.user.name = '';
-     this.user.password = '';
-     this.user.email = ''
-   }
+  back() {
+    this.auth = 'show';
+    this.reset = 'hide';
+    this.user.name = '';
+    this.user.password = '';
+    this.user.email = '';
+  }
 
-  resetPassword(){
-    if(this.user.email != ''){
-      alert("A password reset link has been sent to your email adres: " + this.user.email);
-    }else{
-      alert("enter email");
+  resetPassword() {
+    if (this.user.email !== '') {
+      alert('A password reset link has been sent to your email adres: ' + this.user.email);
+    } else {
+      alert('enter email');
     }
   }
 
-  login(){
-    if(this.user.name != '' || this.user.name != ''){
-      alert("Login Succesful for: " + this.user.name + " - " + this.user.password);;
-    }else{
-      alert("enter username and password");
+  login() {
+    if (this.user.name !== '' || this.user.name !== '') {
+      alert('Login Succesful for: ' + this.user.name + ' - ' + this.user.password);
+    } else {
+      alert('Enter username and password');
     }
   }
 }
--- src/app/shared/services/rest.service.ts
+++ src/app/shared/services/rest.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs';
 import {catchError, tap} from 'rxjs/operators';
 import {share} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
@@ -11,37 +11,11 @@ export class RestService {
 
   readonly ROOT_URL = 'http://localhost:1234/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-<<<<<<< HEAD
   getTotalUsers(): Observable<any> {
-    console.log('Getting total users');
-    /*return this.http.get(this.ROOT_URL + 'users/amount')
-=======
-  //getTotalUsers() {
-    /*console.log('Getting total users');
-    return this.http.get(this.ROOT_URL + 'users/amount')
->>>>>>> ee1ab8e1309ca5d1f064becfe1a882ff9cfb3402
-      .pipe(
-        tap(req => console.log('get-request', req)),               // (6)
-        catchError(                                                // (7)
-          (error) => {
-            console.log(error);
-            alert(error.message);
-            return null;
-          }),
-        share()                                                    // (8)
-      );*/
-<<<<<<< HEAD
-      return this.http.get<any>(this.ROOT_URL + 'users/amount');
-=======
-
-  //}
-
-  getTotalUsers() {
-    console.log(this.http.get(this.ROOT_URL + 'users/amount'));
-    return this.http.get(this.ROOT_URL + 'users/amount');
->>>>>>> ee1ab8e1309ca5d1f064becfe1a882ff9cfb3402
+    return this.http.get<any>(this.ROOT_URL + 'users/amount');
   }
 
 }
