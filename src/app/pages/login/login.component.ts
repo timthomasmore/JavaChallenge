8efac6d899833a5cff1259571c28a105246731b6
Mr_olive123 <michiel336@gmail.com>
2018-11-21 21:19:59 +0100

    login stuff

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,57 +1,63 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../shared/services/auth.service';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  providers: [AuthService]
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    providers: [AuthService]
 })
 
 export class LoginComponent implements OnInit {
-  auth = 'show';
-  reset = 'hide';
-  resetPasswordAPI = 'www.url.com/testURL'; //TE VERVANGEN DOOR LOGIN API
-  loginAPI = 'www.url.com/testLOGIN'; //TE VERVANGEN DOOR LOGIN API
+    auth = 'show';
+    reset = 'hide';
+    resetPasswordAPI = 'www.url.com/testURL'; //TE VERVANGEN DOOR LOGIN API
+    loginAPI = 'www.url.com/testLOGIN'; //TE VERVANGEN DOOR LOGIN API
 
-  user = {
-    name: '',
-    password: '',
-    email: ''
-  };
+    user = {
+        name: '',
+        password: '',
+        email: ''
+    };
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-  }
-
-  forgotPassword() {
-    this.auth = 'hide';
-    this.reset = 'show';
-  }
-
-  back() {
-    this.auth = 'show';
-    this.reset = 'hide';
-    this.user.name = '';
-    this.user.password = '';
-    this.user.email = '';
-  }
-
-  resetPassword() {
-    if (this.user.email !== '') {
-      alert('A password reset link has been sent to your email adres: ' + this.user.email);
-    } else {
-      alert('enter email');
+    constructor(private authService: AuthService, private router: Router) {
     }
-  }
 
-  login() {
-    if (this.user.email !== '' || this.user.email !== '') {
-      this.authService.login(this.user.email, this.user.password);
-    } else {
-      alert('Enter username and password');
+    ngOnInit() {
     }
-  }
+
+    forgotPassword() {
+        this.auth = 'hide';
+        this.reset = 'show';
+    }
+
+    back() {
+        this.auth = 'show';
+        this.reset = 'hide';
+        this.user.name = '';
+        this.user.password = '';
+        this.user.email = '';
+    }
+
+    resetPassword() {
+        if (this.user.email !== '') {
+            alert('A password reset link has been sent to your email adres: ' + this.user.email);
+        } else {
+            alert('enter email');
+        }
+    }
+
+    login() {
+        if (this.user.email !== '' || this.user.email !== '') {
+            this.authService.login(this.user.email, this.user.password).subscribe(
+                () => {
+                    console.log('user is logged in');
+                    this.router.navigate(['pages/index']);
+                }
+            );
+        } else {
+            alert('Enter username and password');
+        }
+    }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,29 +1,50 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
+import * as moment from 'moment';
+import {map} from 'rxjs/operators';
+import 'rxjs/add/operator/shareReplay';
 
 @Injectable()
 export class AuthService {
 
-  readonly ROOT_URL = 'http://localhost:1234/';
+    readonly ROOT_URL = 'http://localhost:1234/';
 
-  constructor(private http: HttpClient, private router: Router) { }
+    constructor(private http: HttpClient, private router: Router) {
+    }
 
-  login(email: string, password: string) {
-    return this.http.post<{ token: string }>(this.ROOT_URL + 'users/login', {email: email, password: password})
-      .subscribe(
-        res => {
-            if (!res.token.startsWith('ERROR')) {
-                localStorage.setItem('token', res.token);
-                console.log(res.token);
-                this.router.navigate(['pages/index']);
-            }else {
-                console.log(res.token);
-                return false;
-            }
-        }
-      );
-  }
+    login(email: string, password: string) {
+        return this.http.post<any>(this.ROOT_URL + 'users/login', {email, password})
+            .pipe(
+                map(res => this.setSession)
+            ).shareReplay();
+    }
+
+    private setSession(authResult) {
+        const expiresAt = moment().add(authResult.expiresIn, 'second');
+
+        localStorage.setItem('id_token', authResult.idToken);
+        localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+        localStorage.removeItem('expires_at');
+    }
+
+    public isLoggedIn() {
+        return moment().isBefore(this.getExpiration());
+    }
+
+    isLoggedOut() {
+        return !this.isLoggedIn();
+    }
+
+    getExpiration() {
+        const expiration = localStorage.getItem('expires_at');
+        const expiresAt = JSON.parse(expiration);
+        return moment(expiresAt);
+    }
 
 }
