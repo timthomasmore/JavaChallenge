b8ff1ba87e17d7bddbc554a42c7e21d873ae5ddf
Mr_olive123 <michiel336@gmail.com>
2018-11-22 18:04:03 +0100

    navbar start

--- src/app/pages/menu.ts
+++ src/app/pages/menu.ts
@@ -1,4 +1,7 @@
-export let MENU_ITEM = [
+import {AuthService} from '../shared/services/auth.service';
+import construct = Reflect.construct;
+
+let defaultMenu = [
     {
         path: 'index',
         title: 'Dashboard',
@@ -14,126 +17,83 @@ export let MENU_ITEM = [
         title: 'Shop',
         icon: 'diamond'
     },
-  {
-    path: 'activiteit',
-    title: 'Activiteit',
-    icon: 'check-square-o',
-    children: [
-      {
-        path: 'historiek',
-        title: 'Historiek'
-      },
-      {
-        path: 'toevoegen',
-        title: 'Toevoegen'
-      }
-    ]
-  },
-  {
-    path: 'admin',
-    title: 'Admin',
-    icon: 'edit',
-    children: [
-      {
-        path: 'rewards',
-        title: 'Beheer shop'
-      },
-      {
-        path: 'activities',
-        title: 'Beheer activiteiten'
-      },
-      {
-        path: 'allowrewards',
-        title: 'Punten toekennen'
-      }
-    ]
-  },
     {
-        path: 'ui',
-        title: 'UI Element',
-        icon: 'paint-brush',
-        children: [
-            {
-                path: 'grid',
-                title: 'Bootstrap Grid'
-            },
-            {
-                path: 'buttons',
-                title: 'Buttons'
-            },
-            {
-                path: 'notification',
-                title: 'Notification'
-            },
-            {
-                path: 'tabs',
-                title: 'Tabs'
-            },
-            {
-                path: 'file-tree',
-                title: 'File Tree'
-            },
-            {
-                path: 'modals',
-                title: 'Modals'
-            },
-            {
-                path: 'progress-bar',
-                title: 'ProgressBar'
-            },
-            /*  {
-                 path: 'loading',
-                 title: 'Loading'
-             }, */
-        ]
-    },
-    {
-        path: 'form',
-        title: 'Forms',
+        path: 'activiteit',
+        title: 'Activiteit',
         icon: 'check-square-o',
         children: [
             {
-                path: 'form-inputs',
-                title: 'Form Inputs'
+                path: 'historiek',
+                title: 'Historiek'
             },
             {
-                path: 'form-layouts',
-                title: 'Form Layouts'
-            },
-            {
-                path: 'file-upload',
-                title: 'File Upload'
-            },
-            {
-                path: 'ng2-select',
-                title: 'Ng2-Select'
-            }
-        ]
-    },
-    {
-        path: 'charts',
-        title: 'Charts',
-        icon: 'bar-chart',
-        children: [
-            {
-                path: 'echarts',
-                title: 'Echarts'
-            }
-        ]
-    },
-    {
-        path: 'table',
-        title: 'Tables',
-        icon: 'table',
-        children: [
-            {
-                path: 'basic-tables',
-                title: 'Basic Tables'
-            },
-            {
-                path: 'data-table',
-                title: 'Data Table'
+                path: 'toevoegen',
+                title: 'Toevoegen'
             }
         ]
     },
 ];
+
+let adminLevel1 = [
+    {
+        path: 'admin',
+        title: 'Admin',
+        icon: 'edit',
+        children: []
+    }
+];
+
+let adminLevel2Rewards = [
+    {
+        path: 'allowrewards',
+        title: 'Punten toekennen'
+    }
+
+];
+
+
+let adminLevel2Shop = [
+    {
+        path: 'rewards',
+        title: 'Beheer shop'
+    },
+];
+
+
+let adminLevel2Assignments = [
+    {
+        path: 'activities',
+        title: 'Beheer activiteiten'
+    },
+];
+
+
+let menu = defaultMenu;
+
+let admin = false;
+if (true) {
+    if (!admin) {
+        admin = true;
+    }
+    adminLevel1[0].children.push(adminLevel2Rewards[0]);
+}
+
+if (true) {
+    if (!admin) {
+        admin = true;
+    }
+    adminLevel1[0].children.push(adminLevel2Shop[0]);
+}
+
+if (true) {
+    if (!admin) {
+        admin = true;
+    }
+    adminLevel1[0].children.push(adminLevel2Assignments[0]);
+}
+
+if (admin) {
+    menu.push(adminLevel1[0]);
+}
+
+export let MENU_ITEM = menu;
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,7 +1,7 @@
 import {Routes, RouterModule} from '@angular/router';
 import {PagesComponent} from './pages.component';
 import {LoginComponent} from './login/login.component';
-import {AuthGuard} from "../guards/auth.guard";
+import {AuthGuard} from '../guards/auth.guard';
 
 export const childRoutes: Routes = [
   {
@@ -13,12 +13,12 @@ export const childRoutes: Routes = [
     component: PagesComponent,
     canActivate: [AuthGuard],
     children: [
-      {path: '', redirectTo: 'index', pathMatch: 'full'},
-      {path: 'index', loadChildren: './index/index.module#IndexModule'},
-      {path: 'editor', loadChildren: './editor/editor.module#EditorModule'},
-      {path: 'shop', loadChildren: './shop/shop.module#ShopModule'},
-      {path: 'profile', loadChildren: './profile/profile.module#ProfileModule'},
-      {path: 'form', loadChildren: './form/form.module#FormModule'},
+        {path: '', redirectTo: 'index', pathMatch: 'full'},
+        {path: 'index', loadChildren: './index/index.module#IndexModule'},
+        {path: 'editor', loadChildren: './editor/editor.module#EditorModule'},
+        {path: 'shop', loadChildren: './shop/shop.module#ShopModule'},
+        {path: 'profile', loadChildren: './profile/profile.module#ProfileModule'},
+        {path: 'form', loadChildren: './form/form.module#FormModule'},
       {path: 'charts', loadChildren: './charts/charts.module#ChartsModule'},
       {path: 'ui', loadChildren: './ui/ui.module#UIModule'},
       {path: 'table', loadChildren: './table/table.module#TableModule'},
