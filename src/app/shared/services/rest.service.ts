f34efc1d0d520614e19538862987f6df32904ecd
jordibols <r0631689@student.thomasmore.be>
2018-11-24 16:42:39 +0100

    Activiteit toevoegen

--- src/app/shared/services/rest.service.ts
+++ src/app/shared/services/rest.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable()
 export class RestService {
@@ -19,6 +20,25 @@ export class RestService {
 
   getRewards(): Observable<any> {
     return this.http.get<any>(this.ROOT_URL + 'rewards');
+  }
+
+  getAssignments(): Observable<any> {
+    return this.http.get<any>(this.ROOT_URL + 'assignments');
+  }
+
+  getUserInfo() {
+    const user_id = jwt_decode( localStorage.getItem('id_token') )['sub'];
+    return this.http.get<any>(this.ROOT_URL + 'users/user/' + user_id );
+  }
+
+  completeAssignment(as, desc) {
+    const body = {
+      assignmentid: as._id,
+      userid: jwt_decode( localStorage.getItem('id_token') )['sub'],
+      description: desc
+    };
+
+    return this.http.post<any>(this.ROOT_URL + 'users/addassignment', body).subscribe( res => console.log(res) );
   }
 
   redeemReward(userid: string, rewardid: string) {
